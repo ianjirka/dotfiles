1f0e946475b5e6ca7131f2ee2ccbaf7017b80334
Ian Jirka <2858147+ianjirka@users.noreply.github.com>
2023-05-04 23:51:08 -0700

    Correct init script management, robustness

--- setup.sh
+++ setup.sh
@@ -23,9 +23,26 @@ cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
+echo "Moving any existing dotfiles from ~ to $olddir"
+
 for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/.$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/.$file
+    local_file=~/.$file
+    backup_file=$olddir/.$file
+    synced_file=$dir/$file
+
+    if [ -e "$local_file" ] && [ ! -L "$local_file" ] 
+    then
+        echo "Backing up $local_file to $olddir"
+        mv $local_file $olddir
+    fi
+    if [ -e "$synced_file" ] 
+    then
+        if [ -L "$local_file" ] 
+        then
+            echo "Removing existing symlink to $local_file"
+            rm $local_file
+        fi
+        echo "Creating symlink to $file in home directory."
+        ln -s $synced_file $local_file
+    fi
 done
